d171d0860434c3756554d17f59fea8bd72072237
alnarolekoala <valentin.frain458@laposte.net>
2021-04-10 23:19:05 +0200

    Insert inputAxes

--- 2D begginner ruby's adventure/Assets/Scripts/RubyController.cs
+++ 2D begginner ruby's adventure/Assets/Scripts/RubyController.cs
@@ -7,14 +7,21 @@ public class RubyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+      
+      
     }
 
     // Update is called once per frame
     void Update()
     {
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+        
         Vector2 position = transform.position;
-        position.x = position.x + 0.01f;
+        
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
         transform.position = position;
+        
     }
 }
